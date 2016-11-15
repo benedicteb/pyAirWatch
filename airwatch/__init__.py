418104b0c2ca69f1b77541669facebbe0f4f9bd4
Benedicte Emilie Brækken <b.e.brakken@usit.uio.no>
2016-11-15 15:06:00 +0100

    Added POST, DELETE, more endpoints verify functions not in class

--- airwatch/__init__.py
+++ airwatch/__init__.py
@@ -24,6 +24,26 @@ class AirWatch(object):
 
         return response.json()
 
+    def post(self, relative_url, data=None, **kwargs):
+        url = urljoin(self.uribase, relative_url)
+
+        if data:
+            response = requests.post(url, auth=self.auth, headers=self.headers,
+                json=data, **kwargs)
+        else:
+            response = requests.post(url, auth=self.auth, headers=self.headers,
+                **kwargs)
+
+        verify_response(response)
+
+    def delete(self, relative_url, **kwargs):
+        url = urljoin(self.uribase, relative_url)
+
+        response = requests.delete(url, auth=self.auth, headers=self.headers,
+            **kwargs)
+
+        verify_response(response)
+
     def get_device_information(self, id, search_by='serialnumber'):
         choices = [
             'serialnumber',
@@ -33,7 +53,7 @@ class AirWatch(object):
             'easid'
         ]
 
-        self._verify_choices(search_by, choices)
+        verify_choices(search_by, choices)
 
         params = {
             'id': id,
@@ -50,7 +70,7 @@ class AirWatch(object):
             'imeinumber'
         ]
 
-        self._verify_choices(search_by, choices)
+        verify_choices(search_by, choices)
 
         params = {
             'id': id,
@@ -66,6 +86,9 @@ class AirWatch(object):
         return self.get('mdm/devices/profiles', params=params)
 
     def get_profile(self, **params):
+        """
+        Searches for all profiles applicable using the query information provided.
+        """
         choices = [
             'type',
             'profilename',
@@ -79,35 +102,113 @@ class AirWatch(object):
             'page'
         ]
 
-        self._verify_params(params, choices)
+        verify_params(params, choices)
 
         return self.get('mdm/profiles/search', params=params)
 
-    def get_profile_details(self, profile_id):
-        return self.get(urljoin('mdm/profiles/', profile_id))
+    def get_profile_details(self, profile_id, version=None):
+        """
+        Gets device profile details identified by the profile Id.
+        """
+        url = urljoin('mdm/profiles/', profile_id)
+
+        if version:
+            url = urljoin(version, url)
+
+        return self.get(url)
+
+    def activate_device_profile(self, profile_id):
+        """
+        Activates a device profile identified by the profile Id.
+        """
+        url = urljoin('mdm/profiles/', profile_id, 'activate')
+        self.post(url)
+
+    def deactivate_device_profile(self, profile_id):
+        """
+        Deactivates a device profile identified by the profile Id.
+        """
+        url = urljoin('mdm/profiles/', profile_id, 'deactivate')
+        self.post(url)
+
+    def delete_device_profile(self, profile_id):
+        """
+        Deletes the device profile identified by the numeric profile Id.
+        """
+        url = urljoin('mdm/profiles/', profile_id)
+        self.delete(url)
+
+    def remove_device_profile(self, profile_id, serial_number=None, udid=None,
+                                macaddress=None, device_id=None):
+        """
+        Removes the profile from the device identified by the device parameter
+        provided.
+
+        Usage:
+        >>> aw.remove_device_profile(1, serial_number='serial_number')
+        >>> aw.remove_device_profile(1, udid='udid')
+        >>> aw.remove_device_profile(1, macaddress='macaddress')
+        >>> aw.remove_device_profile(1, device_id=121)
+        """
+
+        payload = {}
+
+        if serial_number:
+            payload['SerialNumber'] = serial_number
+        elif udid:
+            payload['Udid'] = udid
+        elif macaddress:
+            payload['MacAddress'] = macaddress
+        elif device_id:
+            assert isinstance(device_id, (int, ))
+            payload['DeviceId'] = device_id
+
+        url = urljoin('mdm/profiles/', profile_id, 'remove')
+        self.post(url, data=payload)
+
+    def update_apple_device_profile(self, profile_id, data):
+        """
+        Updates an Apple device profile identified by its numeric ID.
+        """
+        url = 'mdm/profiles/platforms/apple/update'
+
+        assert isinstance(profile_id, (int, ))
+
+        if 'General' in data.keys():
+            data['General']['ProfileId'] = profile_id
+        else:
+            data['General'] = {
+                'ProfileId': profile_id
+            }
+
+        self.post(url, data=data)
 
     def get_organization_group(self, id):
         return self.get('system/groups/%s' % id)
 
-    def _verify_choices(self, search_by, choices):
-        if not search_by in choices:
-            raise Exception('Not able to search by \'%s\', possible choices: %s'\
-                % (search_by, ','.join(choices)))
+def verify_choices(search_by, choices):
+    if not search_by in choices:
+        raise Exception('Not able to search by \'%s\', possible choices: %s'\
+            % (search_by, ','.join(choices)))
 
-    def _verify_params(self, params, choices):
-        if any([p not in choices for p in params.keys()]):
-            raise Exception('Bad parameter given')
+def verify_params(params, choices):
+    if any([p not in choices for p in params.keys()]):
+        raise Exception('Bad parameter given')
 
 def verify_response(response):
-    try:
-        response.json()
-    except Exception:
-        raise Exception('Unable to parse response as json')
-
     if str(response.status_code).startswith('2'):
         return
 
-    errormsg = str(response.status_code) + ' %s' % response.reason
+    errormsg = '%d %s' % (response.status_code, response.reason)
+
+    try:
+        response_json = response.json()
+    except:
+        raise Exception(errormsg)
+
+    if 'Message' in response_json.keys():
+        errormsg += ': %s' % response_json['Message']
+
     raise Exception(errormsg)
 
 def urljoin(*args):
